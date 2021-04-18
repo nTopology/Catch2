3ea5cff47408f822d40c020a970879bac3eb5988
Steve Fortier <stevefortier@ntopology.com>
2021-04-17 23:00:06 -0400

    Add more logging so I can investigate on Jenkins

--- extras/CatchAddTests.cmake
+++ extras/CatchAddTests.cmake
@@ -38,8 +38,6 @@ function(add_command NAME)
   set(script "${script}${NAME}(${_args})\n" PARENT_SCOPE)
 endfunction()
 
-message("Generating test list for '${TEST_EXECUTABLE}'")
-
 # Run test executable to get list of available tests
 if(NOT EXISTS "${TEST_EXECUTABLE}")
   message(FATAL_ERROR
@@ -65,6 +63,9 @@ if(NOT ${result} EQUAL 0)
     "  Output: ${output}\n"
   )
 endif()
+
+message("Found ${result} tests for '${TEST_EXECUTABLE}'")
+message("${output}")
 
 string(REPLACE "\n" ";" output "${output}")
 
